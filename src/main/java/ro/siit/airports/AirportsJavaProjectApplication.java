994e7965c2eae3b3ae937c8da45f311e77a1b5b4
Madalin <madalin.tanase1@gmail.com>
2020-10-31 18:52:11 +0200

    create Spring Boot project structure

---
+++ src/main/java/ro/siit/airports/AirportsJavaProjectApplication.java
@@ -0,0 +1,13 @@
+package ro.siit.airports;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirportsJavaProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirportsJavaProjectApplication.class, args);
+	}
+
+}
