6de9bc2de57b4b7f5fae0d1937391ed357d5e1f8
Madalin <madalin.tanase1@gmail.com>
2020-11-02 00:46:30 +0200

    add home-page and login-page

---
+++ src/main/java/ro/siit/airports/controller/LoginController.java
@@ -0,0 +1,13 @@
+package ro.siit.airports.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+
+    @GetMapping({"/login"})
+    public String displayLoginPage() {
+        return "login";
+    }
+}
